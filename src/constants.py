071dbd7d35a4669795577b76967cb8ff6a00ab95
Ricardo <ricortiz13@gmail.com>
2018-09-06 23:52:43 -0400

    Initial python workspace commit

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+## @file: constants.py
+#  @author: Ricardo Ortiz
+#  @description:
+#  File containing robot constants
+#
+
+## @tuple: pwm_init_stand
+#  @description:
+#  Tuple containing the standup PWM values of each joint
+#
+kStandPWM = tuple([250]*12)
