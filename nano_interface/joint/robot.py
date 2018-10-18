654f432a3f32cc676b27f3d01d3e1fff124d07fe
Ricardo <ricortiz13@gmail.com>
2018-10-17 23:58:11 -0400

    Nano stands with near linear end-effector path with hard-coded IK

--- nano_interface/joint/robot.py
+++ nano_interface/joint/robot.py
@@ -1,13 +1,59 @@
 ## @file: robot.py
 #
+import math
 from link import Link
 from drivers.PCA9685 import PCA9685
+
 
 
 
 class Robot:
     def __init__(self):
         self._legs = {}
+
+    def leg_ik(self,y):
+        l1 = 70.6 #mm
+        l2 = 73.0 #mm
+        theta1 = (l1**2 - l2**2 + y**2)/(2*l1*y)
+        theta1 = math.asin(theta1)
+        
+        angle = math.acos((l1/l2)*math.cos(theta1))
+        theta2 = math.pi - theta1 - angle
+
+        theta1 = math.degrees(theta1)
+        theta2 = math.degrees(theta2)
+
+        theta1 = int(theta1)
+        theta2 = int(theta2)
+        theta2 = 180 - theta2
+
+        # switch = theta1
+        # theta1 = theta2
+        # theta2 = switch
+
+        return theta1, theta2
+
+
+    def controller(self, y, leg='fr'):
+        femur = self._legs[leg]
+        hip = femur.get_child()
+        tibia = hip.get_child()
+
+
+        l1 = 70.6 #mm
+        l2 = 73 #mm
+        if (y*(-1)>=(l1+l2)):
+            y = (l1+l2) - (l1+l2)*0.10
+        
+        theta1, theta2 = self.leg_ik(y)
+
+        print("Theta1: " + str(theta1))
+        print("Theta2: " + str(theta2))
+
+        femur.set_pos(theta1) #controlling hip
+        hip.set_pos(90)#set straight
+        tibia.set_pos(theta2)#tibia
+        
 
     def set_2D_45(self,leg):
         femur = self._legs[leg]
--- nano_interface/joint/robot_test.py
+++ nano_interface/joint/robot_test.py
@@ -6,42 +6,70 @@ if __name__ == "__main__":
     builder.build()
     robot = builder.get_build()
     
+    y = 60
+
     while True:
-        t = 4
+        t = 0.1
+
+        
+
+        if y>130:
+            y=130
+
+        # robot.controller(120,'fl')
+        # time.sleep(t)
+
+        # robot.controller(70, 'fl')
+        # time.sleep(t)
+        print("Y value: " + str(y))
+        robot.controller(y, 'fr')
+        robot.controller(y, 'fl')
+        robot.controller(y, 'br')
+        robot.controller(y, 'bl')
+        time.sleep(t)
+        y+=5
+
+
+
+
         ##R w R
         ##L w L
 
         #reach
-        robot.reach('fr')
-        robot.push('br')
-        robot.push('fl')
-        robot.push('bl')
+
+        # #---------------------
+        # robot.reach('fr')
+        # robot.push('br')
+        # robot.push('fl')
+        # robot.push('bl')
         
-        time.sleep(t)
+        # time.sleep(t)
 
-        #compress
-        robot.compress('fr')
-        robot.reach('fl')
+        # #compress
+        # robot.compress('fr')
+        # robot.reach('fl')
 
-        robot.compress('bl')
+        # robot.compress('bl')
         
-        time.sleep(t)
+        # time.sleep(t)
 
-        #push
-        robot.push('fr')
-        robot.compress('fl')
+        # #push
+        # robot.push('fr')
+        # robot.compress('fl')
 
-        robot.reach('bl')
-        robot.compress('br')
-        time.sleep(t)
+        # robot.reach('bl')
+        # robot.compress('br')
+        # time.sleep(t)
 
-        #retract
-        robot.retract('fr')
-        robot.push('fl')
+        # #retract
+        # robot.retract('fr')
+        # robot.push('fl')
 
-        robot.retract('bl')
-        robot.reach('br')
-        time.sleep(t)
+        # robot.retract('bl')
+        # robot.reach('br')
+        # time.sleep(t)
+
+        #-----------------
 
         #----------------------------------------
         #robot.extend('fr')
