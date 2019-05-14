60bcde6fc802cc579603bbe7592f6026a0e4b118
Ricardo <ricortiz13@gmail.com>
2019-05-13 23:24:04 -0400

    Documenting robot test file. Vertically extend all 4 legs to raise robot to standing position

--- nano_interface/joint/robot_test.py
+++ nano_interface/joint/robot_test.py
@@ -6,82 +6,22 @@ if __name__ == "__main__":
     builder.build()
     robot = builder.get_build()
     
-    y = 60
+    height = 60 #[cm]
 
     while True:
         t = 0.1
 
-        
+        if height>130:
+            height=130
 
-        if y>130:
-            y=130
+        print("height value: " + str(height))
 
-        # robot.controller(120,'fl')
-        # time.sleep(t)
-
-        # robot.controller(70, 'fl')
-        # time.sleep(t)
-        print("Y value: " + str(y))
-        robot.controller(y, 'fr')
-        robot.controller(y, 'fl')
-        robot.controller(y, 'br')
-        robot.controller(y, 'bl')
+        # Extend all legs of the robot to the specified height
+        # This loop will make the robot rise with all four legs incrementing 
+        # with a vertical end-effector path
+        robot.controller(height, 'fr')
+        robot.controller(height, 'fl')
+        robot.controller(height, 'br')
+        robot.controller(height, 'bl')
         time.sleep(t)
-        y+=5
-
-
-
-
-        ##R w R
-        ##L w L
-
-        #reach
-
-        # #---------------------
-        # robot.reach('fr')
-        # robot.push('br')
-        # robot.push('fl')
-        # robot.push('bl')
-        
-        # time.sleep(t)
-
-        # #compress
-        # robot.compress('fr')
-        # robot.reach('fl')
-
-        # robot.compress('bl')
-        
-        # time.sleep(t)
-
-        # #push
-        # robot.push('fr')
-        # robot.compress('fl')
-
-        # robot.reach('bl')
-        # robot.compress('br')
-        # time.sleep(t)
-
-        # #retract
-        # robot.retract('fr')
-        # robot.push('fl')
-
-        # robot.retract('bl')
-        # robot.reach('br')
-        # time.sleep(t)
-
-        #-----------------
-
-        #----------------------------------------
-        #robot.extend('fr')
-        #robot.extend('fl')
-        #robot.fold('br')
-        #robot.fold('bl')
-
-        #time.sleep(1)
-    
-        #robot.extend('br')
-        #robot.extend('bl')
-        #robot.fold('fr')
-        #robot.fold('fl')
-
-        #time.sleep(1)
+        height+=5
